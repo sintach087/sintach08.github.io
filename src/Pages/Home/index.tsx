f9b5b16319e2518019cefbfa2cd54c2d90e175af
sintach087 <avenillajeanson08@gmail.com>
2022-04-15 03:24:31 +0800

    Update

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.scss';
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+            <div className='contruction'>
+                Under construction
+            </div>
+        </div>
+    )
+}
+
+export default Home;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { Home } from './Pages';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <></>
+    <Routes>
+      <Route index element={<Navigate to="home" />} />
+      <Route path="home" element={<Home />} />
+      <Route path="*" element={<Navigate to="home" />} />
+    </Routes>
   </BrowserRouter>
 );
-
-reportWebVitals();
